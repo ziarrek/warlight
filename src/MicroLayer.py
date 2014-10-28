faa103e974cc38aa6b446b67b9c8339fdfaf94b7
Maciej Ziarkowski <ziarrek@gmail.com>
2014-10-28 17:14:37 +0800

    Fixed example code in MicroLayer.py

--- src/MicroLayer.py
+++ src/MicroLayer.py
@@ -15,16 +15,15 @@ class MicroLayer(BotLayer):
 
 	def attack_transfer(self, info, input):
 		troop_numbers = []
-		if info.has_key('you_decide') and not info['you_decide']:
-			if info.has_key('troops'):
-				troop_numbers = info['troops']
-			else:
-				troop_numbers = [10, 20, 12]
+		if info.has_key('you_decide') and not info['you_decide'] and info.has_key('troops'):
+			troop_numbers = info['troops']
+		else:
+			troop_numbers = [10, 20, 12]
 		
 		return {
 			'attack_transfers': [
 				(1,2,troop_numbers[0]),
 				(4,5,troop_numbers[1]),
-				(7,3,troop_numbers[3])
+				(7,3,troop_numbers[2])
 			]
 		}
