a4741f53621692e51f1245ab19c5e5f5dd393f70
Maciej Ziarkowski <ziarrek@gmail.com>
2014-11-09 18:11:05 +0800

    Added debugging messages for checking the neighbours ownership in MicroLayer

--- src/MicroLayer.py
+++ src/MicroLayer.py
@@ -2,6 +2,7 @@ from BotLayer import BotLayer
 
 from util import Map, Region, SuperRegion, Random, get_other_player
 from math import ceil
+from sys import stderr
 
 
 
@@ -29,7 +30,8 @@ class MicroLayer(BotLayer):
 
 		self.intended_moves = []
 		placements = []
-
+		stderr.write('your_bot: '+your_bot)
+		stderr.write('\n\nour regions:'+ ''.join([reg.id for reg in world.regions if reg.owner == player]))
 		# distribute the armies by giving half of the remaining number
 		# to the next region, stop when ran out of armies
 		# with 7 armies available, 3 regions will be populated
@@ -38,13 +40,17 @@ class MicroLayer(BotLayer):
 			region_action = region[2]
 			region_obj = world.get_region_by_id(region_id)
 			placement_region_id =''
+			stderr.write('checked region: '+region_id+'\n')
 			if region_action == 'attack':
 				candidates = []
 				for neighbour in region_obj.neighbours:
+					stderr.write('neighbour id: '+neighbour.id+'\n')
+					stderr.write('is_fog: '+neighbour.is_fog+" "+'owner: '+neighbour.owner+'\n')
 					if not neighbour.is_fog and neighbour.owner == player:
 						candidates.append(neighbour)
-				placement_region = max(candidates,key=lambda x:x.troop_count)
-				placement_region_id = placement_region.id
+				if candidates:
+					placement_region = max(candidates,key=lambda x:x.troop_count)
+					placement_region_id = placement_region.id
 				# TODO add intended_move handling
 			elif region_action == 'defend':
 				placement_region_id = region_id
@@ -77,11 +83,12 @@ class MicroLayer(BotLayer):
 				for neighbour in region_obj.neighbours:
 					if not neighbour.is_fog and neighbour.owner == player:
 						candidates.append(neighbour)
-				move_region = max(candidates,key=lambda x:x.troop_count)
-				move_region_id = move_region.id
-				move_troops_count = move_region.troops_count - 1
-				if move_troops_count:
-					attack_transfers.append((move_region_id, region_id, move_troops_count))
+				if candidates:
+					move_region = max(candidates,key=lambda x:x.troop_count)
+					move_region_id = move_region.id
+					move_troops_count = move_region.troops_count - 1
+					if move_troops_count:
+						attack_transfers.append((move_region_id, region_id, move_troops_count))
 
 		return {
 			'attack_transfers': attack_transfers
