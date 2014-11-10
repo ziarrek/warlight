6b572ac00b0d65117d134789e16d4312874ff630
Maciej Ziarkowski <ziarrek@gmail.com>
2014-11-11 05:32:20 +0800

    Improved new version of MicroLayer, can attacks one region from multiple regions

--- src/MicroLayer.py
+++ src/MicroLayer.py
@@ -18,6 +18,7 @@ def get_defend_armies(attacker_count, risky=0.5):
 def get_attack_casualties(attacker_count,risky=0.5):
 	return int(round(attacker_count * 0.5))
 
+
 class MicroLayer(BotLayer):
 
 	def __init__(self):
@@ -54,6 +55,16 @@ class MicroLayer(BotLayer):
 		# planned troop placements
 		placements_dict = defaultdict(lambda: 0)
 
+		def get_army_split(unused_existing, allowed_assignment, needed):
+			usage = min(needed, unused_existing)
+			assignment = 0
+			unfulfilled = 0
+			if unused_existing < needed:
+				more_needed = needed - unused_existing
+				assignment = min(more_needed, allowed_assignment)
+				if assignment < more_needed:
+					unfulfilled = more_needed - assignment
+			return (usage, assignment, unfulfilled)
 
 		for region_tup in regions:
 			region_id, region_priority, region_action = region_tup
@@ -63,36 +74,30 @@ class MicroLayer(BotLayer):
 				opponent_troops = region.troop_count
 				needed_attacking_troops = get_attack_armies(opponent_troops)
 				allowed_troops_assignment = min(needed_attacking_troops, int(round(starting_armies*region_priority/priorities_total)))
-				candidates = [neighbour for neighbour in region.neighbours if neighbour.owner == player]
-				candidate_unused_troops = [c.troop_count - used_troops[c.id] for c in candidates]
-				# get neighbour with maximum unused troops
-				chosen_i, chosen_unused_troops = max(enumerate(candidate_unused_troops), key=itemgetter(1))
-				chosen = candidates[chosen_i]
-				unused = candidate_unused_troops[chosen_i]
-				used_troops[chosen.id] += min(needed_attacking_troops, unused)
-				if needed_attacking_troops > unused:
-					needed_more = needed_attacking_troops - unused
-					# used_troops[chosen.id] += unused
-					final_assignment = min(needed_more, allowed_troops_assignment)
-					placements_dict[chosen.id] += final_assignment
-					left_armies -= final_assignment
+				candidates = sorted([neighbour for neighbour in region.neighbours if neighbour.owner == player],key=lambda x:x.troop_count,reverse=True)
+				candidate_unused_troops = {c.id: c.troop_count - used_troops[c.id] for c in candidates}
 
-					if needed_more > allowed_troops_assignment:
-						# skip the movement, wait for better times...
-						continue
-						# placements_dict[chosen.id] += allowed_troops_assignment
-						# left_armies -= allowed_troops_assignment
-					# else:
-						# placements_dict[chosen.id] += needed_more
-						# left_armies -= needed_more
-						# moves.append( (chosen.id, region.id, needed_attacking_troops) )
-				# else:
-					# used_troops[chosen.id] += needed_attacking_troops
+				proposed_moves = []
 
-				# add the intended move to the list
-				moves.append( (chosen.id, region.id, needed_attacking_troops) )
-				pred_opp_casualties[region.id] += get_attack_casualties(needed_attacking_troops)
+				needed_more = needed_attacking_troops
+				allowed_more = allowed_troops_assignment
+				for chosen in candidates:
+					if needed_more <= 0 or allowed_more <= 0:
+						break
+					unused = candidate_unused_troops[chosen.id]
+					usage, assignment, needed_more = get_army_split(unused,allowed_more,needed_more)
 
+					used_troops[chosen.id] += usage
+					placements_dict[chosen.id] += assignment
+					allowed_more -= assignment
+					left_armies -= assignment
+					proposed_moves.append( (chosen.id, region.id, usage+assignment) )
+
+				# if all attacks have the chance of killing more than half of the opponent's forces, attack
+				if needed_more <= needed_attacking_troops * 0.5:
+					moves += proposed_moves
+					troops_used = needed_attacking_troops - needed_more
+					pred_opp_casualties[region.id] += get_attack_casualties(troops_used)
 
 			elif region_action == 'defend':
 				neighbour_opponents = [neigh for neigh in region.neighbours if neigh.owner == opponent]
@@ -125,21 +130,18 @@ class MicroLayer(BotLayer):
 				# move[2] += amount
 				placements_dict[move[0]] += amount
 				left_armies -= amount
-
-		# while left_armies:
-		# 	for move in moves:
-		# 		move[2] += 1
-		# 		placements_dict[move[0]] += 1
-		# 		left_armies -= 1
-		# 		if not left_armies:
-		# 			break
+		if left_armies:
+			# redistribute unused armies to placements
+			redistribution = int(math.ceil(left_armies *1.0/len(placements_dict)))
+			for placement_region_id in placements_dict:
+				if not left_armies:
+					break
+				amount = min(redistribution, left_armies)
+				placements_dict[placement_region_id] += amount
+				left_armies -= amount
 
 		placements = [(reg_id, troop_count) for reg_id, troop_count in placements_dict.iteritems() if troop_count > 0]
 
-		stderr.write('\n\n')
-		pp.pprint(placements)
-		stderr.write('\n\n')
-		stderr.write('place_armies: left_armies: '+str(left_armies))
 		self.intended_moves = moves
 
 		return {
@@ -147,32 +149,14 @@ class MicroLayer(BotLayer):
 		}
 
 	def attack_transfer(self, info, input):
-		regions = self.regions
-		world = info['world']
+		# regions = self.regions
+		# world = info['world']
 
-		player = info['your_bot']
-		opponent = get_other_player(player)
+		# player = info['your_bot']
+		# opponent = get_other_player(player)
 
 
 		attack_transfers = [move for move in self.intended_moves]
-		# attack_transfers = []
-		# for region_tup in regions:
-		# 	region_id = region_tup[0]
-		# 	region_action = region_tup[2]
-		# 	region = world.get_region_by_id(region_id)
-
-		# 	if region_action == 'attack':
-		# 		# TODO add intended_moves use
-		# 		candidates = []
-		# 		for neighbour in region.neighbours:
-		# 			if not neighbour.is_fog and neighbour.owner == player:
-		# 				candidates.append(neighbour)
-		# 		if candidates:
-		# 			move_region = max(candidates,key=lambda x:x.troop_count)
-		# 			move_region_id = move_region.id
-		# 			move_troops_count = move_region.troop_count - 1
-		# 			if move_troops_count:
-		# 				attack_transfers.append((move_region_id, region_id, move_troops_count))
 
 		return {
 			'attack_transfers': attack_transfers
